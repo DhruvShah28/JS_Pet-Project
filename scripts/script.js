b4c7a68a796c3a6939375e61f2558a86cbf752e5
Dhruv Shah <69303943+DhruvShah28@users.noreply.github.com>
2024-12-02 20:31:29 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ function accordian(disp) {
     var change = document.getElementById(disp); 
     var allsection = document.getElementsByClassName("menu-items");
     var allarrows = document.getElementsByClassName("down-arrow"); 
+
     
     for (let i = 0; i < allsection.length; i++) {
         allarrows[i].style.transform = "rotate(45deg)";
@@ -18,7 +19,7 @@ function accordian(disp) {
         arrow.style.transform = "rotate(225deg)";
         arrow.style.transition = "transform 100ms linear";
     }
-}
+};
 
 
 window.onload = function(){
@@ -28,32 +29,34 @@ var burgerking_details = {
     menu1: "Cheese Burger",
     menu2: "Tex Max Whopper Burger",
     menu3: "BBQ Bacon King",
-    menu4: "Roadhouse Burger"
-}
+    menu4: "Roadhouse Burger",
+};
 
 var subway_details = {
     menuhead: "Classic Subs",
     menu1: "Veggie Delight",
     menu2: "Veggie Patty",
     menu3: "Great Canadian Club",
-    menu4: "Chicken Rancher"
-}
+    menu4: "Chicken Rancher",
+
+};
 
 var redswan_details = {
     menuhead: "Special Pizzas",
     menu1: "Cheese Pizza",
     menu2: "Chicken Pizza",
     menu3: "Double Veggie Pizza",
-    menu4: "Classic Canadian Pizza"
-}
+    menu4: "Classic Canadian Pizza",
+};
 
 var pizzap_details = {
     menuhead: "Gourmet Thins",
     menu1: "Creamy Mushroom Florentine",
     menu2: "Hot Honey",
     menu3: "Pesto Amore Pizza",
-    menu4: "Buffalo Chicken Tikka Pizza"
-}
+    menu4: "Buffalo Chicken Tikka Pizza",
+
+};
 
 
 var mcd_details = {
@@ -61,8 +64,9 @@ var mcd_details = {
     menu1: "Cheese Burger",
     menu2: "Ham Burger",
     menu3: "Quater Pounder BLT Burger",
-    menu4: "McDouble Burger"
-}
+    menu4: "McDouble Burger",
+
+};
     var formhandler = document.forms.rest_from;
 
     var btn = document.getElementById("compare-btn");
@@ -75,11 +79,58 @@ var mcd_details = {
 
     var error = document.getElementById("error-text");
 
-    labelhead = document.getElementById("changemenu");
-    label1 = document.getElementById("change1");
-    label2 = document.getElementById("change2");
-    label3 = document.getElementById("change3");
-    label4 = document.getElementById("change4");
+    var labelhead = document.getElementById("changemenu");
+    var label1 = document.getElementById("change1");
+    var label2 = document.getElementById("change2");
+    var label3 = document.getElementById("change3");
+    var label4 = document.getElementById("change4");
+
+    var card = document.getElementsByClassName("full-card");
+
+    var randomval1 = document.getElementById("randomval-1");
+    var randomval2 = document.getElementById("randomval-2");
+    var randomval3 = document.getElementById("randomval-3");
+
+    var rval1 = (Math.random() * (30 - 20 + 1)) + 20;
+    var rval2 = (Math.random() * (30 - 20 + 1)) + 20;
+    var rval3 = (Math.random() * (30 - 20 + 1)) + 20;
+
+    var saved = document.getElementById("saved_amt");
+
+    randomval1.innerHTML = rval1.toFixed(2);
+    randomval2.innerHTML = rval2.toFixed(2);
+    randomval3.innerHTML = rval3.toFixed(2);
+
+    checklowest();
+
+    function checklowest(){
+        var highval = rval1;
+        var lowval = rval1;
+        if (rval1 < rval2 && rval1 < rval3) {
+            randomval1.closest(".full-card").id = "lowest-val";
+        } else if (rval2 < rval1 && rval2 < rval3) {
+            randomval2.closest(".full-card").id = "lowest-val";
+        } else {
+            randomval3.closest(".full-card").id = "lowest-val";
+        }
+
+        if (rval2 < lowval) {
+            lowval = rval2;
+        }
+        if (rval3 < lowval) {
+            lowval = rval3;
+        }
+    
+        if (rval2 > highval) {
+            highval = rval2;
+        }
+        if (rval3 > highval) {
+            highval = rval3;
+        }
+
+        saved.innerHTML = (highval - lowval).toFixed(2);
+
+    }
 
     setTimeout(() => {
         if(!formhandler.restaurant.onchange || formhandler.restaurant.options[formhandler.restaurant.selectedIndex].value === "XX"){
